a7a69cd37dee559402754a7cdb2ed7438b28ce36
SamarthTumdi <samarth.tumdi@gmail.com>
2022-08-28 18:13:57 +0530

    Correct manual Duration Slider finished

---
+++ sliderManualControl.js
@@ -0,0 +1,14 @@
+let slider = document.querySelector('.duration-slider');
+let sliderFill = document.querySelector('.sliderFill');
+
+let sliderWidth = parseFloat(window.getComputedStyle(slider).getPropertyValue('width'))
+let thumbWidth = 25;
+let widthIncrement = (sliderWidth - thumbWidth) / slider.max;
+let prevSliderValue = slider.value;
+
+slider.addEventListener('input', function() {
+  console.log(slider.value)
+  sliderFill.style.width = `${parseFloat(window.getComputedStyle(sliderFill).getPropertyValue('width')) + (widthIncrement * (slider.value - prevSliderValue))}px`;
+  prevSliderValue = slider.value;
+  // The multiplier approach allows for width reduction as well, as well as tapped events along with drag events!
+})
