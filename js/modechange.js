0160caee9deeddf6d521ce01fab681ad6e66a477
SamarthTumdi <samarth.tumdi@gmail.com>
2022-09-17 12:50:20 +0530

    Fixed bug in change mode

--- js/modechange.js
+++ js/modechange.js
@@ -1,5 +1,8 @@
 themeDivRef = document.getElementById('theme-selector');
 bodyRef = document.getElementsByTagName("body")[0];
+currMode = bodyRef.classList[0] || 'light'
+console.log(currMode)
+themeDivRef.classList.add(currMode);
 
 themeDivRef.addEventListener('click', () => {
   themeDivRef.classList.toggle('dark');
